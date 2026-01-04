41b0ab9069b390c4cb9649e2f1ef915614de0fdc
Enisel Kunch <enisel_12@abv.bg>
2026-01-04 17:51:15 +0200

    Fix Flake8 W292: add two spaces before inline comment

--- src/test_app.py
+++ src/test_app.py
@@ -4,4 +4,4 @@ from src.app import app
 def test_health():
     client = app.test_client()
     response = client.get("/health")
-    assert response.data == b"OK" # nosec B101
+    assert response.data == b"OK"  # nosec B101
