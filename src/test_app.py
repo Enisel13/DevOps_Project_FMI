d1f40bfc247db5eb9532e9dc206ce342ac913f4c
Enisel Kunch <enisel_12@abv.bg>
2026-01-06 01:59:30 +0200

    Modified test_app

--- src/test_app.py
+++ src/test_app.py
@@ -1,7 +1,14 @@
 from src.app import app
 
 
+# def test_health():
+#     client = app.test_client()
+#     response = client.get("/health")
+#     assert response.data == b"OK"  # nosec B101
+
 def test_health():
     client = app.test_client()
     response = client.get("/health")
-    assert response.data == b"OK"  # nosec B101
+
+    assert response.status_code == 200
+    assert b"OK Super" in response.data
